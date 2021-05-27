30fbecfed10d47743739ea5476ea951c3c3b6329
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 23:21:41 -0600

    fixing style to Sigin.jsx

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -7,13 +7,9 @@ class SignIn extends React.Component{
         return(
             <React.Fragment>
                 <Hero h={"15vh"} foto={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <SignInForm></SignInForm>
-                        </div>
-                    </div>
-                </div>
+                
+                <SignInForm></SignInForm>
+                        
             </React.Fragment>
         );
     }
