cfbd102b31fc240e38b08c103c21c3bc62214ecb
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 21:57:12 -0600

    Changed button message and add link to button

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -8,8 +8,8 @@ const Main=(props) =>{
         <React.Fragment>
             <div className="Main">
                 <div className="Main__content">
-                    <h1 className="Main__title">{props.title}</h1>
-                    <Button contentText={"Login"}></Button>
+                    <h1 className="Main__title">{props.title || "Welcome"}</h1>
+                    <Button contentText={"Lets Start"} link={"/new"}></Button>
                 </div>
                 <Hero h={"88vh"} foto={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
             </div>
