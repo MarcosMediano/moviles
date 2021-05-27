6a1bc2202fe699c7f7d1d3721c4d737dcda52e4a
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 23:41:50 -0600

    finish NotFound

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import Hero from "../../components/Hero"
+import './NotFound.css'
 
 class NotFound extends React.Component{
     render(){
         return(
             <React.Fragment>
                 <Hero h={"15vh"} foto={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
-                
+                <div className="Found">
+                <img className="NotFound__img" src="https://onlinezebra.com/wp-content/uploads/2019/01/error-404-not-found.jpg"></img>
+                </div>
                 
                         
             </React.Fragment>
