971da9bd22870576e01e93f4bfb85e1d42ba9c0d
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 23:13:50 -0600

    Added Login form and its css

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+import './style/LoginForm.css'
+
+class LoginForm extends Component{
+    render(){
+        return(
+            <div className="LoginForm m-5">
+                <form>
+                    <div className="form-group mb-2">
+                        <label className="mb-1">
+                            E-Mail
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="form-group mb-2">
+                        <label className="mb-1">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="form-control"
+                        />
+                    <button type="submit" className="Login__button">
+                            Log In
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
