d2255406e5f252125d5bffc567eea0b6d0f8826f
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 23:26:33 -0600

    create new folder and file pages for error 404

---
+++ src/pages/notfound/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Hero from "../../components/Hero"
+
+class NotFound extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Hero h={"15vh"} foto={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
+                
+                
+                        
+            </React.Fragment>
+        );
+    }
+}
+
+export default NotFound;
