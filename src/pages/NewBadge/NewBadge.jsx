360b1efd94cba1deb847dafb8f6e88f1f31abe1e
MarcosMediano <marcosmediano@gmail.com>
2021-05-26 20:17:04 -0600

    adding state in NewBadge.jsx to send information to the badge

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Hero from "../../components/Hero"
-import Badge from "../../components/Badge";
+import Badge from "../../components/Badge"
+import BadgeForm from "../../components/BadgeForm"
 import "./NewBadge.css"
 
 class NewBadge extends React.Component{
@@ -9,7 +10,7 @@ class NewBadge extends React.Component{
         loading :false,
         error: null,
         form:{
-            header_picutre:"",
+            header_picture:"",
             profile_picture:"",
             name:"",
             age:"",
@@ -19,25 +20,45 @@ class NewBadge extends React.Component{
             post:""
         }
     }
+
+    handleChange=event=>{
+        this.setState({
+            form:{
+                ...this.state.form,
+                [event.target.name]: event.target.value,
+            }
+        })
+        
+    }
+    handleSubmit =async(event)=>{
+        event.preventDefault();
+    }
+
     render(){
         return(
             <React.Fragment>
                 <Hero h={"15vh"} foto={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}></Hero>
                 <div className="container">
-                    <div className="col">
-                        <Badge 
-                            profile_picture={this.state.form.profile_picture || "https://images.pexels.com/photos/3229336/pexels-photo-3229336.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                            header_picture={this.state.form.header_picutre || "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
-                            name={this.state.form.name || "Janet Williams "}
-                            age={this.state.form.age || "21"}
-                            city={this.state.form.city || "Tokyo Japan"}
-                            followers={this.state.form.followers || "120k"}
-                            likes={this.state.form.likes || "3M"}
-                            post={this.state.form.post || "420"}
-                        ></Badge>
-                    </div>
-                    <div className="col">
-
+                    <div className="row">
+                        <div className="col">
+                            <Badge 
+                                profile_picture={this.state.form.profile_picture || "https://images.pexels.com/photos/3229336/pexels-photo-3229336.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                                header_picture={this.state.form.header_picture || "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"}
+                                name={this.state.form.name || "Janet Williams "}
+                                age={this.state.form.age || "21"}
+                                city={this.state.form.city || "Tokyo Japan"}
+                                followers={this.state.form.followers || "120k"}
+                                likes={this.state.form.likes || "3M"}
+                                post={this.state.form.post || "420"}
+                            ></Badge>
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm 
+                            onChange={this.handleChange}
+                            onSubmit={this.handleSubmit}
+                            formValues={this.state.form}>    
+                            </BadgeForm>
+                        </div>
                     </div>
                 </div>
                 
